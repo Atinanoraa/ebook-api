249e8295f8b0a60669fbf5c2fd206ba5e18d9e65
Atinanoraa <atinanhy@gmail.com>
2021-09-24 21:34:50 +0700

    Challenge Author

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+
 Route::resource('/books', BookController::class);
+Route::resource('/authors', AuthorController::class);
